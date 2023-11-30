11760c8d4fb280eb093a4a4d96f394336710a1f4
cgadd21 <126418690+cgadd21@users.noreply.github.com>
2023-11-29 22:38:10 -0500

    DataService complete

--- Services/DataService/DataService.java
+++ Services/DataService/DataService.java
@@ -1,6 +1,37 @@
 package Services.DataService;
 
+import java.sql.*;
+
 public class DataService implements IDataService
 {
+    private Connection conn;
+
+    @Override
+    public Connection getConnection() 
+    {
+        return conn;
+    }
     
+    @Override
+    public void connect() 
+    {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/project", "root", "student");
+        } 
+        catch (ClassNotFoundException cnfe) {} 
+        catch (SQLException sqle) {}
+    }
+
+    @Override
+    public void close() 
+    {
+        try 
+        {
+            if (conn != null) conn.close();
+        } 
+        catch (SQLException sqle) {}
+    }
+
 }
--- Services/DataService/IDataService.java
+++ Services/DataService/IDataService.java
@@ -1,6 +1,10 @@
 package Services.DataService;
 
+import java.sql.*;
+
 public interface IDataService 
 {
-    
+    void connect();
+    void close();
+    Connection getConnection();
 }
